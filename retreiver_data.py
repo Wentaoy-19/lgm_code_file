fe0218ddce6083162fd5eaf733792c77ef202dc3
Wentaoy-19 <15726938186@163.com>
2022-07-13 11:32:53 -0500

    model

--- retreiver_data.py
+++ retreiver_data.py
@@ -1,8 +1,22 @@
 import json 
 import torch 
-from datasets import load_dataset 
+from datasets import load_dataset,load_from_disk
 import random
 from transformers import GPT2Tokenizer,OPTModel 
+from torch.utils.data import Dataset 
+
+
+class retreiver_dataset(Dataset):
+    def __init__(self,path):
+        self.hf_dataset = load_from_disk(path)
+    def __len__(self):
+        return len(self.hf_dataset)
+    def __getitem__(self,idx):
+        q_text = self.hf_dataset[idx]['question']
+        c_emds = torch.tensor(self.hf_dataset[idx]['context'])
+        return (q_text, c_emds)
+
+
 
 def create_neg_ids(pos_id:int, num_data:int,num_gen:int):
     i = 0
@@ -17,42 +31,59 @@ def create_neg_ids(pos_id:int, num_data:int,num_gen:int):
     return result
 
 def convert_squad_dic():
+    device = torch.device("cuda:1")
     tokenizer = GPT2Tokenizer.from_pretrained('facebook/opt-350m')
-    opt_model = OPTModel.from_pretrained('facebook/opt-1.3b')
+    opt_model = OPTModel.from_pretrained('facebook/opt-1.3b').to(device)
     squad_dataset = load_dataset('squad')
-    question_list = []
-    context_list = []
+    # question_list = []
+    # context_list = []
+    total_list = [] 
     size_squad = len(squad_dataset['train'])
     for i in range(size_squad):
         psg_text = []
         psg_emd = []
-        question_list.append(squad_dataset['train'][i]['question'])
+        # question_list.append(squad_dataset['train'][i]['question'])
         psg_text.append(squad_dataset['train'][i]['context'])
         neg_psg_ids = create_neg_ids(pos_id=i, num_data= size_squad, num_gen= 10)
         for j in neg_psg_ids:
             psg_text.append(squad_dataset['train'][j]['context'])
         for text in psg_text:
-            input_ids = tokenizer(text,return_tensors="pt")['input_ids']
-            emd = opt_model(input_ids = input_ids)['last_hidden_state'].detach()[0][-1]
+            input_ids = tokenizer(text,return_tensors="pt")['input_ids'].to(device)
+            emd = opt_model(input_ids = input_ids)['last_hidden_state'].cpu().detach()[0][-1]
             psg_emd.append(emd)
         psg_emd = torch.stack(psg_emd)
-        context_list.append(psg_emd)
-    return {"question": question_list, "context": context_list}
+        total_list.append({'question':squad_dataset['train'][i]['question'],'context': psg_emd.tolist()})
+        print("Current: ", i, "total: ", size_squad)
+        # context_list.append(psg_emd.tolist())
+    return {"data": total_list}
+    # return {"question": question_list, "context": context_list}
 
 def convert_dic_json(dic:dict,path):
     with open(path, "w") as outfile:
         json.dump(dic, outfile)
 
-def main_squad_json(path = "data/json_data/sample.json"):
+def convert_json_dataset(json_path,save_path):
+    temp_dataset = load_dataset("json", data_files=json_path, split="train",field = "data")
+    temp_dataset.save_to_disk(save_path)
+
+def main_squad_json(path = "../data/json_data/sample.json"):
     dic = convert_squad_dic()
     convert_dic_json(dic,path)
 
+def main_squad_json_dataset(json_path="../data/json_data/retreiver.json", dataset_path = "../data/convert_dataset/retreiver" ):
+    dic = convert_squad_dic()
+    convert_dic_json(dic,json_path)
+    convert_json_dataset(json_path,dataset_path)
+
+    
+
 def load_json_dataset(path):
-    temp_dataset = load_dataset("json", data_files=path, split="train")
+    temp_dataset = load_dataset("json", data_files=path, split="train",field = "data")
     return temp_dataset
 
 
 if __name__ == "__main__":
-    main_squad_json(path = "data/json_data/sample.json")
+    main_squad_json_dataset()
+    # main_squad_json(path = "../data/json_data/sample.json")
 
 
--- retreiver_model.py
+++ retreiver_model.py
@@ -1,35 +1,49 @@
 import torch 
 import torch.nn as nn 
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 from transformers import AutoModel, OPTModel, GPT2Tokenizer
 from datasets import load_dataset, load_from_disk
 class opt_retreiver(torch.nn.Module):
-    def __init__(self):
+    def __init__(self,weight_path):
         super(opt_retreiver,self).__init__()
         self.dim = 2048
         self.device = torch.device("cuda:0")
-        self.weights_path ='/raid/projects/wentaoy4/model_file/models--facebook--opt-1.3b/snapshots/c8fd4232a5df1e87e06d5cbb9e066c5a114cd4ee'
+        self.weights_path = weight_path
         
         self.lm_model = OPTModel.from_pretrained(self.weights_path).to(self.device)
+        self.tokenizer = GPT2Tokenizer.from_pretrained(self.weights_path)
         self.header = nn.Linear(self.dim,self.dim).to(self.device)
         
         self.lm_out = None 
         self.model_out = None 
-        
         self.loss = None 
-        
+        self.lr = 1e-6
+        self.optimizer = optim.Adam(self.parameters(),lr= self.lr) 
     # def _forward(self,inputs):
     #     input_ids = inputs.to(self.device)
     #     self.lm_out = self.lm_model(input_ids = input_ids)['last_hidden_state'][0][-1]
     #     self.model_out = self.header(self.lm_out) 
     #     return self.model_out
+
+    def main_train(self,dataloader:DataLoader):
+        self.lr = 1e-6
+        self.epoches = 1
+        self.optimizer = optim.Adam(self.parameters(),lr= self.lr)
+        for n in range(self.epoches):
+            for i,x in enumerate(dataloader):
+                loss = self.train_batch_forward(x)
+                self.train_batch_backward(loss)
+                print(loss)
     
-    def _forward(self,inputs):
-        self.lm_out = self.lm_model(input_ids = inputs['input_ids'].to(self.device), attention_mask = inputs['attention_mask'].to(self.device))['last_hidden_state'][:,-1,:]
-        self.model_out = self.header(self.lm_out)
-        return self.model_out
+    def forward(self,inputs):
+        # self.lm_out = self.lm_model(input_ids = inputs['input_ids'].to(self.device), attention_mask = inputs['attention_mask'].to(self.device))['last_hidden_state'][:,-1,:].detach()
+        # self.model_out = self.header(self.lm_out)
+        # return self.model_out
+        lm_out = self.lm_model(input_ids = inputs['input_ids'].to(self.device), attention_mask = inputs['attention_mask'].to(self.device))['last_hidden_state'][:,-1,:].detach()
+        model_out = self.header(lm_out)
+        return model_out
             
-        
-    
     def dot_product_scores(self,q_vectors: torch.tensor, ctx_vectors: torch.tensor) -> torch.tensor:
         """
         calculates q->ctx scores for every row in ctx_vector
@@ -51,15 +65,29 @@ class opt_retreiver(torch.nn.Module):
         )
         return loss
     
+    def train_batch_forward(self, batch_data):
+        size = len(batch_data[0])
+        temp_loss = 0
+        for i in range(size):
+            temp_loss += self.train_data_forward(batch_data[0][i], batch_data[1][i])
+        return temp_loss/size
+
+
+    def train_data_forward(self,input_text:str, ctx_vectors: torch.tensor):
+        inputs = self.tokenizer(input_text,return_tensors = 'pt')
+        return self.train_forward(inputs, ctx_vectors)
+    
     def train_forward(self,inputs, ctx_vectors:torch.tensor):
-        q_vectors = self._forward(inputs).to(self.device)
+        q_vectors = self.forward(inputs).to(self.device)
         ctx_vectors = ctx_vectors.to(self.device)
         pos_ids = torch.tensor([0]*int(q_vectors.size()[0])).to(self.device)
-        self.loss = self.NLLloss(q_vectors,ctx_vectors,pos_ids)
-        return self.loss 
+        loss = self.NLLloss(q_vectors,ctx_vectors,pos_ids)
+        return loss 
     
-    def train_backward(self,loss):
-        
-    
-    
-    def batch_train(self):
+    def train_batch_backward(self,loss:torch.tensor):
+        loss.backward()
+        self.optimizer.step()
+        self.optimizer.zero_grad()
+        self.zero_grad()
+
+
